b50f0c7895579b10de7a6524a08e631fb053fe23
diepreyealalibo <95508346+diepreyealalibo@users.noreply.github.com>
2021-12-03 19:23:54 -0500

    Create student.java

---
+++ student.java
@@ -0,0 +1,52 @@
+
+import java.util.Vector;
+
+
+public class Student extends Person{
+private String id;
+private Vector<Course> courses; // contains all courses the student is registered in
+
+public String getName() {
+return name;
+}
+
+public String getId() {
+return id;
+}
+
+// constructor
+public Student(String id, String name) {
+// initialize name and id. Also initialize the Vector
+this.id = id;
+this.name = name;
+courses = new Vector<>();
+}
+
+// registering the student for the given course
+public void registerFor(Course course) {
+if (!courses.contains(course)) {
+courses.add(course);
+course.classList.add(this);
+course.getDept().registerList.add(this);
+}
+}
+
+public boolean isRegisteredInCourse(Course course) {
+return courses.contains(course);
+}
+
+public String toString() {
+// return a string representation of a student using the following format:
+// 100234546 John McDonald
+// Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
+// StringBuilder sb = new StringBuilder(id).append(" ").append(name).append("\nRegistered courses: ").append(courses.get(0).getCode()).append(" ").append(courses.get(0).getTitle());
+
+StringBuilder sb = new StringBuilder(id).append(" ").append(name).append("\nRegistered courses: ");
+  
+for (Course course : courses) {
+sb.append(", ");
+sb.append(course.getCode()).append(" ").append(course.getTitle());
+}
+return sb.toString();
+}
+}
